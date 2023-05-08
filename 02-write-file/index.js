94d61a8063af83fbfa02f1687656a37fb6984ccd
Murat <cryptmensis@gmail.com>
2023-05-08 19:13:42 +0300

    feat: task 2 completed

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const filePath = './02-write-file/text.txt';
+
+console.log('Hello, please enter some text:');
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
+    fs.appendFile(filePath, input + '\n', (err) => {
+      if (err) throw err;
+      console.log(`"${input}" was added to ${filePath}`);
+      console.log('Please enter more text, or type "exit" to quit:');
+    });
+  }
+});
+
+rl.on('close', () => {
+  console.log('Goodbye!');
+});
