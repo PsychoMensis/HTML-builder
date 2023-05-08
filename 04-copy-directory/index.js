91a2663e4679dfd127662b8cef3cf1b67c48f718
Murat <cryptmensis@gmail.com>
2023-05-08 19:43:15 +0300

    feat: task 4 completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir(src, dest) {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) throw err;
+
+    fs.readdir(src, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+
+      files.forEach(file => {
+        const srcPath = path.join(src, file.name);
+        const destPath = path.join(dest, file.name);
+
+        if (file.isDirectory()) {
+          copyDir(srcPath, destPath);
+        } else {
+          fs.copyFile(srcPath, destPath, (err) => {
+            if (err) throw err;
+            console.log(`${path.basename(srcPath)}`);
+          });
+        }
+      });
+    });
+  });
+}
+
+copyDir('./04-copy-directory/files', './04-copy-directory/files-copy');
