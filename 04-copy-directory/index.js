1e4e85bf08d8017958d718006a2a9f717b54da2e
Murat <cryptmensis@gmail.com>
2023-05-10 13:24:28 +0300

    fix: task 4 folder deletion

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+function removeDirectory(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    fs.readdirSync(dirPath).forEach(file => {
+      const filePath = path.join(dirPath, file);
+      if (fs.lstatSync(filePath).isDirectory()) {
+        removeDirectory(filePath);
+      } else {
+        fs.unlinkSync(filePath);
+      }
+    });
+    fs.rmdirSync(dirPath);
+  }
+}
+
 function copyDir(src, dest) {
+  removeDirectory(dest);
+
   fs.mkdir(dest, { recursive: true }, (err) => {
     if (err) throw err;
 
